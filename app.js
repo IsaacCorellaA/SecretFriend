115ab1cf5c713e5b4ea22f9ec51e75657f022c88
Isaac Corella Alvarado <143538498+IsaacCorellaA@users.noreply.github.com>
2025-02-17 11:30:21 -0600

    Array Amigos/Función agregarAmigos()

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+
+
+function agregarAmigo(){
+    
+    nombre = document.getElementById("amigo").value.trim(); // con el trim me aseguro de que no haya espacios
+    if (nombre == ""){
+        alert("El nombre no puede quedar vacío, por favor intente de nuevo.");
+        return;
+    }else{
+        amigos.push(nombre);
+        //console.log("Ingresé el amigo a la lista")
+        alert("El nombre se registró");
+    } 
+
+        //console.log("Presiono")
+
+        
+}
